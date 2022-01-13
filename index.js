0c75c851376452cec6476c1378f96b52abae2d72
Pritesh <iampritesh13@gmail.com>
2022-01-13 16:20:36 +0530

    initial commit

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ const path = require('path');
 console.log(" Path of __dirname is = " + __dirname );
 console.log(" Path of __filename is = " + __filename + "\n");
 
-const home = path.join( __dirname, '/index.html' );
-const about = path.join( __dirname, '/about.html' );
-const blog = path.join( __dirname, '/blog.html' );
-const service = path.join( __dirname, '/service.html' );
-const contact = path.join( __dirname, '/contact.html' );
+const home = path.join( __dirname, 
+    '/html_files/index.html' );
 
-console.log(" value of home is = " + home + "\n");
+const about = path.join( __dirname, 
+    '/html_files/about.html' );
 
+const blog = path.join( __dirname, 
+    '/html_files/blog.html' );
+
+const service = path.join( __dirname, 
+    '/html_files/service.html' );
+
+const contact = path.join( __dirname, 
+    '/html_files/contact.html' );
 
 // define PORT number  
 const PORT = process.env.PORT || 4200;
