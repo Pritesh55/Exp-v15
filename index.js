09b5c725526036be24472db8de54a07408bd5f6b
Pritesh <iampritesh13@gmail.com>
2022-01-13 13:25:43 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// index.js file
-
 // imPORT Third party module :: Express module... 
 const express = require('express');
 const app = express();
@@ -8,9 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 4200;
 
 app.get("/", (req, resp) => {
-    resp.send("<h1> Jay shree ram...</h1>");
+    resp.send("<h1> Jay Shree ram...</h1>");
 });
 
 app.listen(PORT, () => {
-    console.log("server is working on PORT no :: " + PORT + "\n" + "http://localhost:" + PORT);
+    console.log("server is working on PORT no :: "
+     + PORT + "\n" + "http://localhost:" + PORT);
 });
+
